629d9b1b0a8ef254afe6c47c1b7b271b75990946
ngc <nora.gonzalo@gmail.com>
2021-02-28 12:08:10 +0100

    ADD snakeGame

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import EyeTest from './components/eyeTest/eyeTest'
-import Footer from './components/Footer/Footer'
+// import EyeTest from './components/eyeTest/eyeTest'
+import Footer from './components/Footer/Footer';
+import SnakeGame from './components/snake/SnakeGame'
  function App() {
 return(
   <>
   {/* <EyeTest></EyeTest> */}
+  <SnakeGame></SnakeGame>
   <Footer></Footer>
   </>
 )
