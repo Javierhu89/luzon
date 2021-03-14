8f6f2d71245ea3c7b1c1620ce9521726aaedf3b6
Javi <javierpahissa@gmail.com>
2021-03-14 20:29:26 +0100

    Eligiendo jugador

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import Game1 from "../Game1/Game1";
-import Header from '../Header/Header';
 import './Game.scss';
 import { Link } from 'react-router-dom';
 import IconButton from '@material-ui/core/IconButton';
@@ -12,79 +10,33 @@ class Game extends Component {
     super(props)
   
     this.state = {
-       valueicono: '/assets/img/chico.png',
-       valuenombre: 'Javier',
-       valueedad: 31,
-       icono: ['/assets/img/chico.png', '/assets/img/hombre-de-negocios.png', '/assets/img/jefe.png','/assets/img/mujer.png', '/assets/img/nina.png'],
-       nombres: ['Javier','Pedro','Juán','Ana','Julia'],
-       edades: [31,36,26,25,45],
-       llave: 0, 
-       level: 1
+       
     }
   }
-
-  cambiarLevel = () => {
-    let valor = document.getElementById('level');
-    let eleccion = valor.selectedIndex + 1;
-    this.setState({level:eleccion})
-    
-  }
-  cambiarPersonajeIzq = () => {
-    if(this.state.llave===4){
-      this.state.llave=0
-    } else {
-      this.state.llave += 1;
-    }
-    this.setState({valueicono: this.state.icono[this.state.llave]})
-    this.setState({valuenombre: this.state.nombres[this.state.llave]})
-    this.setState({valueedad: this.state.edades[this.state.llave]})
-    }
-    cambiarPersonajeDer = () => {
-      if(this.state.llave===0){
-        this.state.llave=4
-      } else {
-        this.state.llave -= 1;
-      }
-    this.setState({valueicono: this.state.icono[this.state.llave]})
-    this.setState({valuenombre: this.state.nombres[this.state.llave]})
-    this.setState({valueedad: this.state.edades[this.state.llave]})
-      }
 
   render() {
-    return <>
-    <Header />
-    <p className="combatir"><b>El juego que se creó para combatir la ELA</b></p>
-    <p className="difcolor">Para empezar elige un jugador y nivel</p>
-    <div className="conjuntoiconos">
-      <div className="seleccion">
-      <IconButton aria-label="delete" size="medium" onClick={() => {this.cambiarPersonajeIzq()}}><ArrowLeftwardIcon fontSize="large" /></IconButton>
-      <img className="iconos" alt="icono1" id="icono1" src={process.env.PUBLIC_URL + this.state.valueicono} /> 
-      <IconButton aria-label="delete" size="medium" onClick={() => {this.cambiarPersonajeDer()}}><ArrowRightwardIcon fontSize="large" /></IconButton>
+    return <div>
+    <h3 className="choose">Elige tu jugador</h3>
+    <div className='recuadro'>
+      <div className='jugadores'>
+        <div className='parrafos'>
+        <p className='descripcion'>Buenas tardes, soy María</p>
+        <p className='descripcion'>Llevo 6 o 7 meses desde que he empezado a notar síntomas.<span className="negrita"> Leer mas...</span></p>
+          </div><img className="iconos" alt="Foto de María" className="foto" src={process.env.PUBLIC_URL + '/assets/img/maria.png'} />
       </div>
-      <div className="nomico">
-      <p className="estado"><b>Nombre: {this.state.valuenombre}</b></p> <p className="estado"><b>Edad: {this.state.valueedad}</b></p>
-      </div>
+      <p className='edad'>María 20 años</p>
     </div>
-      <select name="selectorLevel" id="level" onChange={this.cambiarLevel}>
-          <option key="1" value='1'>Level 1</option>
-          <option key="2" value='2'>Level 2</option>
-          <option key="3" value='3'>Level 3</option>
-          <option key="4" value='4'>Level 4</option>
-          <option key="5" value='5'>Level 5</option>
-          <option key="6" value='6'>Level 6</option>
-      </select>
-      <div className="seleccion">
+    <div className='recuadro'>
+      <div className='jugadores'>
+        <div className='parrafos'>
+        <p className='descripcion'>Buenas tardes, soy Javier</p>
+        <p className='descripcion'>Llevo 9 meses haciéndome pruebas y sigo sin saber qué es lo que me pasa?!.<span className="negrita"> Leer mas...</span></p>
+          </div><img alt="Foto de Javier" className="foto javi" src={process.env.PUBLIC_URL + '/assets/img/javier.png'} />
+      </div>
+      <p className='edad javiedad'>Javier 35 años</p>
+    </div>
       <Nav />
-      {/* <Link to={'/'}>
-        <button className="home">
-          Home
-        </button> 
-  </Link> */}
-      <Link to={{ pathname:`/phase${this.state.level}` , state: {icono:this.state.valueicono}}}>
-        {this.state.icono?<button className="phase1">¡Jugar!</button>:<></>}
-      </Link> 
     </div>
-    </>
   }
 }
 
